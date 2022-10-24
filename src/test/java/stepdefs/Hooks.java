1738fcd8b479de6bc3920d0c7a856406dba152c1
sebastianrubioj <sebastianrubioj@hotmail.com>
2022-10-24 18:31:02 -0500

    gitignore updated

--- src/test/java/stepdefs/Hooks.java
+++ src/test/java/stepdefs/Hooks.java
@@ -3,17 +3,17 @@ package stepdefs;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.LoginPage;
 
 public class Hooks {
    private static WebDriver driver;
-   //private LoginPage loginPage;
 
    @Before
    public void initialize(){
-      System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver");
+      WebDriverManager.chromedriver().setup();
       driver = new ChromeDriver();
       driver.manage().window().maximize();
    }
